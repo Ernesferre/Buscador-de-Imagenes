24d2679d2d1451f6b8a2ac3f20ad173cdecfba52
Ernesferre <70835371+Ernesferre@users.noreply.github.com>
2021-04-05 11:24:52 -0300

    mostrando imagenes en pantalla

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import Formulario from './components/Formulario'
 import { useState, useEffect } from "react"
+import ListadoImagenes from './components/ListadoImagenes'
 
 function App() {
 
   //State de la App
   const [busqueda, guardarBusqueda ] = useState('');
+  const [imagenes, guardarImagenes ] = useState([]);
+
 
   useEffect (() => {
     const consultarApi = async () => {
         if (busqueda === '') return;
 
-        const imagenesPorPagina = 30;
+        const imagenesPorPagina = 10;
         const key = '21014840-b15efc7b60557e87cc9cf574d'
         const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
 
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
 
-        console.log(resultado.hits);
+        guardarImagenes(resultado.hits);
     }
     consultarApi(); 
 
@@ -27,13 +30,19 @@ function App() {
   return (
     <div className="container">
       <div className="jumbotron">
-        <div className="lead text-center">
-          <p> Buscador de Imagenes </p>
+        <p className="lead text-center"> Buscador de Imagenes </p>
+        
           <Formulario
             guardarBusqueda={guardarBusqueda}
           />
-        </div>
-      </div>     
+        
+      </div>  
+      
+      <div className="row justify-content-center">
+        <ListadoImagenes
+          imagenes={imagenes}
+        />
+      </div>
     </div>
   );
 }
